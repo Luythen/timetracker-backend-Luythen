8363a6a975ddbab12ce40784aab21785d131731c
Luythen <andenklasson@gmail.com>
2026-04-28 10:40:58 +0200

    Init

---
+++ src/test/java/com/github/Luythen/timetracker_backend/TimetrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Luythen.timetracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
