8c1ec0f010461576e26fdc81c2d65ba76058cf4d
Luythen <andenklasson@gmail.com>
2026-04-29 19:26:49 +0200

    add/TimeTrackerService

---
+++ src/main/java/com/github/Luythen/timetracker_backend/Service/TimeTrackerService.java
@@ -0,0 +1,40 @@
+package com.github.Luythen.timetracker_backend.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.github.Luythen.timetracker_backend.Model.TimeTrackerModel;
+import com.github.Luythen.timetracker_backend.Repository.TimeTrackerRepository;
+
+@Service
+public class TimeTrackerService {
+
+    private final TimeTrackerRepository timeTrackerRepository;
+
+    public TimeTrackerService (TimeTrackerRepository timeTrackerRepository) {
+        this.timeTrackerRepository = timeTrackerRepository;
+    }
+
+    public TimeTrackerModel start (TimeTrackerModel timeTrackerModel) {
+        return timeTrackerRepository.insert(timeTrackerModel);
+    }
+
+    public void stop (String timeTrackerID, LocalDate stopDate) {
+        timeTrackerRepository.findAndSetStopDateById(timeTrackerID, stopDate);
+    }
+
+    public Optional<TimeTrackerModel> findById (String id) {
+        return timeTrackerRepository.findById(id);
+    }
+
+    public List<TimeTrackerModel> getUserList (String userID) {
+        return timeTrackerRepository.findAllByUserID(userID);
+    }
+
+    public List<TimeTrackerModel> getList () {
+        return timeTrackerRepository.findAll();
+    }
+}
