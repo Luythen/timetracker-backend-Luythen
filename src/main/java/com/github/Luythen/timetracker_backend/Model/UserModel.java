a8aa2584bfd519216835a466b3da509012288ddf
Luythen <andenklasson@gmail.com>
2026-04-30 19:22:11 +0200

    add/UserModel

---
+++ src/main/java/com/github/Luythen/timetracker_backend/Model/UserModel.java
@@ -0,0 +1,63 @@
+package com.github.Luythen.timetracker_backend.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Document(collection = "User")
+public class UserModel implements UserDetails {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("TM_" + getRole()));
+       
+        return authorities;
+    }
+    
+}
