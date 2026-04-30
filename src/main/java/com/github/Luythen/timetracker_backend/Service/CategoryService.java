270912710e55487fd4c09bef001e24f081eaa5a3
Luythen <andenklasson@gmail.com>
2026-04-30 17:56:40 +0200

    add/CategoryService

---
+++ src/main/java/com/github/Luythen/timetracker_backend/Service/CategoryService.java
@@ -0,0 +1,36 @@
+package com.github.Luythen.timetracker_backend.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.github.Luythen.timetracker_backend.Model.CategoryModel;
+import com.github.Luythen.timetracker_backend.Repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService (CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Optional<CategoryModel> findCategoryById (String id) {
+        return categoryRepository.findById(id);
+    }
+
+    public List<CategoryModel> getAllCategorysByUserID (String userID) {
+        return categoryRepository.findAllByUserID(userID);
+    }
+
+    public void updateCategoryName (String id, String newName) {
+        categoryRepository.findAndSetNameById(id, newName);
+    }
+
+    public CategoryModel createCategory (CategoryModel categoryModel) {
+        return categoryRepository.insert(categoryModel);
+    }
+
+}
