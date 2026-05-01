8650ce94cfb3848fe2752bcb0512ae5d56baf7ae
Luythen <andenklasson@gmail.com>
2026-05-01 22:03:31 +0200

    fix/UserModel

--- src/main/java/com/github/Luythen/timetracker_backend/Model/UserModel.java
+++ src/main/java/com/github/Luythen/timetracker_backend/Model/UserModel.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,8 +16,11 @@ public class UserModel implements UserDetails {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String username;
+    @Indexed(unique = true)
     private String email;
+    
     private String password;
     private String role;
 
