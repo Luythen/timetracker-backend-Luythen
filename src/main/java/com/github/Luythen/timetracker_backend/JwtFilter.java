9db273144e9558295259e7f4a4b99d889bb673f5
Luythen <andenklasson@gmail.com>
2026-05-01 18:36:03 +0200

    add/JwtFilter

---
+++ src/main/java/com/github/Luythen/timetracker_backend/JwtFilter.java
@@ -0,0 +1,53 @@
+package com.github.Luythen.timetracker_backend;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.github.Luythen.timetracker_backend.Model.UserModel;
+import com.github.Luythen.timetracker_backend.Service.JWTService;
+import com.github.Luythen.timetracker_backend.Service.UserDetailServiceImpl;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    UserDetailServiceImpl userDetailServiceImpl;
+
+    @Autowired
+    JWTService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("Token".equals(cookie.getName())) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (jwt != null) {
+            UserModel userDetails = (UserModel) userDetailServiceImpl.loadUserByUsername(jwtService.getClaims(jwt).getSubject());
+
+           if (jwtService.isTokenValid(jwt, userDetails)) {
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+           }
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+}
