ce3ffcc8277aa5a341aa82de8ea74ac0e13b401b
Luythen <andenklasson@gmail.com>
2026-05-01 21:59:30 +0200

    fix/CategoryRepository

--- src/main/java/com/github/Luythen/timetracker_backend/Repository/CategoryRepository.java
+++ src/main/java/com/github/Luythen/timetracker_backend/Repository/CategoryRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Update;
 
 import com.github.Luythen.timetracker_backend.Model.CategoryModel;
+import com.github.Luythen.timetracker_backend.Model.UserModel;
 
 public interface CategoryRepository extends MongoRepository<CategoryModel, String> {
-    List<CategoryModel> findAllByUserID (String userID);
+    List<CategoryModel> findAllByUserModel (UserModel userModel);
 
     @Update("{'set': {'name': ?1}}")
     public void findAndSetNameById (String id, String newName);
